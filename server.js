670fb98053fc9adc71f625a8427eed346b9c83a5
srivatsavd2 <136217897+srivatsavd2@users.noreply.github.com>
2024-09-13 10:39:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors package
-const webrtc = require('wrtc');
-
 const app = express();
+const bodyParser = require('body-parser');
+const { RTCPeerConnection, RTCSessionDescription } = require('wrtc');
+
 let senderStream;
 
-// Enable CORS for all origins (adjust for production as needed)
-app.use(cors());
-
-app.use(express.static('public'));
+app.use(express.static('public')); // Serve static files from 'public' directory
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/consumer', async (req, res) => {
-    const peer = new webrtc.RTCPeerConnection({
+// Handle incoming video streams
+app.post("/broadcast", async (req, res) => {
+    const peer = new RTCPeerConnection({
         iceServers: [
-            { urls: 'stun:stun.stunprotocol.org' }
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
+        ]
+    });
+    peer.ontrack = (e) => handleTrackEvent(e, peer);
+
+    const desc = new RTCSessionDescription(req.body.sdp);
+    await peer.setRemoteDescription(desc);
+    const answer = await peer.createAnswer();
+    await peer.setLocalDescription(answer);
+
+    res.json({ sdp: peer.localDescription });
+});
+
+app.post("/consumer", async (req, res) => {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: "stun:stun.stunprotocol.org"
+            }
         ]
     });
 
-    const desc = new webrtc.RTCSessionDescription(req.body.sdp);
+    const desc = new RTCSessionDescription(req.body.sdp);
     await peer.setRemoteDescription(desc);
 
     if (senderStream) {
@@ -33,24 +50,8 @@ app.post('/consumer', async (req, res) => {
     res.json({ sdp: peer.localDescription });
 });
 
-app.post('/broadcast', async (req, res) => {
-    const peer = new webrtc.RTCPeerConnection({
-        iceServers: [
-            { urls: 'stun:stun.stunprotocol.org' }
-        ]
-    });
-
-    peer.ontrack = (event) => {
-        senderStream = event.streams[0];
-    };
-
-    const desc = new webrtc.RTCSessionDescription(req.body.sdp);
-    await peer.setRemoteDescription(desc);
-
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-
-    res.json({ sdp: peer.localDescription });
-});
+function handleTrackEvent(e) {
+    senderStream = e.streams[0];
+}
 
 app.listen(5000, () => console.log('Server started on port 5000'));
